a44b19321842e79bfefd76239981585d84cbb9d1
danhkaito <danh.kaitokid@gmail.com>
2023-02-17 11:44:40 +0700

    a

--- model/KSOM/CNode.py
+++ model/KSOM/CNode.py
@@ -16,3 +16,19 @@ class CNode(Node):
     def addPNode(self, inputNode):
         self.PNodes.append(inputNode)
         return
+    
+    def print_PNodes(self):
+        true_label = 0
+        false_label = 0
+        if len(self.PNodes) == 0:
+            print("No corpus")
+            return
+        for node in self.PNodes:
+            print(node)
+            if node.label == True:
+                true_label += 1
+            else:
+                false_label += 1
+        print("SUMMARY: " + str(true_label) + " , " + str(false_label) + "\n")
+
+
